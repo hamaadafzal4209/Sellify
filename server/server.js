b687278ab7701bae7a6a27bd81fece40dba798c1
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-07 09:55:26 -0700

    connect database and error handling

--- server/server.js
+++ server/server.js
@@ -1,9 +1,44 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDatabase from "./db/Database.js";
+import dotenv from "dotenv";
+import { errorHandlerMiddleware } from "./middleware/error.js";
 
+// config
 const app = express();
-dotenv.config();
 
-app.listen(process.env.PORT || 8000 , () => {
-    console.log(`Server is running on PORT ${process.env.PORT || 8000}`);
-})
+dotenv.config({
+  path: "./server/config/.env",
+});
+
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+connectDatabase();
+
+// import routes
+
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandled promise rejection`);
+});
+
+// Error handling middleware
+app.use(errorHandlerMiddleware);
+
+// create server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
