b25fab5d1eeac3726c1dbf763228bd2035ace585
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-08 12:01:57 -0700

    integrate clerk for authentication

--- client/app/components/Layout/Navbar.tsx
+++ client/app/components/Layout/Navbar.tsx
@@ -124,8 +124,8 @@ const Navbar: FC<Props> = () => {
             onClick={toggleDropdown}
             className="flex-shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
           >
-            All categories
-            <BiChevronDown className="ml-2.5" size={20} />
+            All
+            <BiChevronDown className="ml-1.5" size={20} />
           </button>
           {dropdownOpen && (
             <div className="max-h-96 overflow-y-auto absolute left-0 top-0 mt-12 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,6 +2,13 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./components/Layout/Navbar";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,13 +32,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar/>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Navbar>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Navbar>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
