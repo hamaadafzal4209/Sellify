a86e3a03a49cc2f1a79334b5f6179ee2072a5e0e
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-09 22:13:47 -0700

    add image slider in hero section

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,13 @@
 import React, { FC } from 'react'
+import Hero from './components/Home/Hero'
 
+// eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {}
 
-const Navbar:FC<Props> = (Props) => {
+const Navbar:FC<Props> = () => {
   return (
     <div>
-      
+      <Hero/>
     </div>
   )
 }
