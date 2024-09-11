3298a92738c7680f925d838a10fd3ee29b19dd22
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-11 14:03:04 -0700

    add redux persist

--- client/app/components/ReduxProvider.tsx
+++ client/app/components/ReduxProvider.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from '../redux/store'
+import { store, persistor } from '../redux/store';
+import { useEffect, useState } from 'react';
 
 export default function ReduxProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    // Check if window is available, meaning we're on the client-side
+    setIsClient(typeof window !== 'undefined');
+  }, []);
+
+  if (!isClient) {
+    return null; // Don't render anything on the server
+  }
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
