95f45d768158bbb37f9c2edc333111dc9eb4bf27
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-10 21:00:11 -0700

    create login page UI

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,11 +1,98 @@
-import React, { FC } from 'react'
+"use client"
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { FC, useState } from 'react';
+import { FiEye, FiEyeOff } from 'react-icons/fi'; // Import icons
 
-type Props = {}
+// eslint-disable-next-line @typescript-eslint/ban-types
+type Props = {};
 
-const page:FC<Props> = () => {
+const Page: FC<Props> = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>Login Page</div>
-  )
-}
+    <div>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Image width={200} height={100} className="mx-auto" src="/logo.png" alt="Sellify" />
+          <h2 className="mt-4 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
 
-export default page
+        <div className="mt-4 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-primary hover:text-primary">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2 relative">
+                <input
+                  id="password"
+                  name="password"
+                  type={showPassword ? 'text' : 'password'}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6"
+                />
+                {/* Show or hide password icon */}
+                <button
+                  type="button"
+                  onClick={togglePasswordVisibility}
+                  className="absolute inset-y-0 right-0 flex items-center pr-3 text-sm text-gray-600"
+                >
+                  {showPassword ? <FiEyeOff /> : <FiEye />} {/* Toggle between eye and eye-off */}
+                </button>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-darkPrimary focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-4 text-center text-sm text-gray-500">
+            {"Don't have account? "}
+            <Link href="/register" className="font-semibold leading-6 text-primary hover:text-primary">
+              Register a new account
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
