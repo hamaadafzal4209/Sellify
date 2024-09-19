43bae3d9d08c831682dbb9594b2bb8552de42d04
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-19 16:21:07 -0700

    create register page ui

---
+++ client/app/register/page.tsx
@@ -0,0 +1,187 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { useState } from "react";
+
+const SignUpPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
+  return (
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign Up for Sellify
+        </h2>
+      </div>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form method="POST">
+          {/* Username Field */}
+          <div>
+            <label
+              htmlFor="username"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Username
+            </label>
+            <div className="mt-1">
+              <input
+                id="username"
+                name="username"
+                type="text"
+                required
+                autoComplete="username"
+                className="form-input"
+              />
+            </div>
+          </div>
+
+          <div className="mt-4">
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Email address
+            </label>
+            <div className="mt-1">
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autoComplete="email"
+                className="form-input"
+              />
+            </div>
+          </div>
+
+          <div className="mt-4 relative">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Password
+            </label>
+            <div className="mt-1 relative">
+              <input
+                id="password"
+                name="password"
+                type={showPassword ? "text" : "password"}
+                required
+                autoComplete="new-password"
+                className="form-input pr-10"
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-primary-500 focus:outline-none"
+              >
+                {showPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+          </div>
+
+          <div className="mt-4 relative">
+            <label
+              htmlFor="confirm-password"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Confirm Password
+            </label>
+            <div className="mt-1 relative">
+              <input
+                id="confirm-password"
+                name="confirm-password"
+                type={showConfirmPassword ? "text" : "password"}
+                required
+                autoComplete="new-password"
+                className="form-input pr-10"
+              />
+              <button
+                type="button"
+                onClick={toggleConfirmPasswordVisibility}
+                className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-primary-500 focus:outline-none"
+              >
+                {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+              </button>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between my-4">
+            <div className="flex items-center">
+              <input
+                id="terms"
+                name="terms"
+                type="checkbox"
+                className="h-4 w-4 text-primary-500 border-gray-300 rounded focus:ring-primary-500"
+              />
+              <label
+                htmlFor="terms"
+                className="ml-2 block text-sm text-gray-900"
+              >
+                I agree to the{" "}
+                <Link
+                  href=""
+                  className="text-primary-500 hover:text-primary-600"
+                >
+                  terms and conditions
+                </Link>
+              </label>
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" className="submit-full-button">
+              Sign Up
+            </button>
+          </div>
+        </form>
+
+        {/* Google Sign Up Button */}
+
+        <div className="relative flex items-center justify-center my-8">
+          <div className="w-full h-[1px] bg-gray-300"></div>
+          <span className="px-4 text-sm font-medium text-gray-500 bg-white mx-4">
+            or
+          </span>
+          <div className="w-full h-[1px] bg-gray-300"></div>
+        </div>
+
+        <div className="mt-6">
+          <button className="flex w-full items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-semibold leading-6 text-gray-600 shadow-sm hover:bg-gray-50 transition-all duration-200">
+            <Image
+              width={20}
+              height={20}
+              src="/assets/google.png"
+              alt="Google Icon"
+              className="mr-3 h-5 w-5 object-contain"
+            />
+            <span className="text-gray-600">Sign up with Google</span>
+          </button>
+        </div>
+
+        <p className="mt-6 text-center text-sm text-gray-500">
+          Already have an account?{" "}
+          <Link
+            href="/login"
+            className="font-semibold leading-6 text-primary-500 hover:text-primary-600"
+          >
+            Sign in
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
