d2fc540cfadd2f240832eba0f9f2a7446d20c25d
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-19 11:31:48 -0700

    server setup

--- server/index.js
+++ server/index.js
@@ -1,8 +1,45 @@
-const express = require('express')
-const app = express()
+import express from "express";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors";
+import connectDatabase from "./db/Database.js";
+import dotenv from "dotenv";
+import { errorHandlerMiddleware } from "./middleware/error.js";
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+// config
+const app = express();
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({
+    path: "config/.env",
+  });
+}
 
-app.listen(8000)
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+connectDatabase();
+
+// import routes
+
+
+// unhandled promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`Shutting down the server for ${err.message}`);
+  console.log(`shutting down the server for unhandled promise rejection`);
+});
+
+// Error handling middleware
+app.use(errorHandlerMiddleware);
+
+// create server
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT}`);
+});
