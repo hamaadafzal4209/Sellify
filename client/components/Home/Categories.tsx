67fca7bb01feebc119adf04e8f4af62b23808d3e
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-20 20:24:06 -0700

    complete shop category component

--- client/components/Home/Categories.tsx
+++ client/components/Home/Categories.tsx
@@ -24,29 +24,32 @@ const categories = [
 
 const CategoriesSection = () => {
   return (
-    <section className="main-container py-12">
-      <div className="flex items-center justify-between gap-4">
-        <h2 className="mb-8 text-2xl font-extrabold text-gray-900 text-center">
-          Shop by Category
-        </h2>
-        <button className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">
+    <section className="main-container py-12 bg-gray-50">
+      <div className="flex items-center justify-between gap-4 mb-6">
+        <h2 className="text-3xl font-bold text-gray-900">Shop by Category</h2>
+        <button className="hidden sm:block py-2.5 px-5 text-sm font-medium text-primary-600 bg-white border border-primary-500 rounded-lg hover:bg-primary-50 hover:text-white transition-colors hover:bg-primary-500">
           See all categories
         </button>
       </div>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {categories.map((category) => (
           <div
             key={category.name}
-            className="flex flex-col items-center justify-center p-6 border rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 bg-white"
+            className="flex flex-col items-center justify-center p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 cursor-pointer"
           >
             <div className="text-primary-500 mb-4">
               <category.icon className="h-12 w-12" />
             </div>
-            <h3 className="text-lg font-semibold text-gray-800">
+            <h3 className="text-lg font-semibold text-gray-900">
               {category.name}
             </h3>
           </div>
         ))}
+      </div>
+      <div className="mt-6 flex items-center justify-center  sm:hidden">
+        <button className="py-2.5 px-5 text-sm font-medium text-primary-600 bg-white border border-primary-500 rounded-lg hover:bg-primary-50 hover:text-white transition-colors hover:bg-primary-500">
+          See all categories
+        </button>
       </div>
     </section>
   );
