3aba601d97486f38767e260e5222fbab36117696
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-20 21:00:00 -0700

    create best selling component

---
+++ client/components/Home/BestSelling.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from "react";
+import ProductCard from "../Products/ProductCard";
+
+const BestSellingProducts = () => {
+  return (
+    <section className="main-container py-12">
+      <div className="flex items-center justify-between gap-4 mb-6">
+        <h2 className="section-heading">Best Deals</h2>
+      </div>
+
+      {/* Product Grid */}
+      <div className="card-container gap-6">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
+    </section>
+  );
+};
+
+export default BestSellingProducts;
