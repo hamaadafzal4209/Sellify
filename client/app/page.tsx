41c36ce9e7cde12450f6c895281e994d534db108
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-20 21:02:58 -0700

    commit

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,16 @@
-import BestSellingProducts from '@/components/Home/BestSelling'
-import CategoriesSection from '@/components/Home/Categories'
-import Hero from '@/components/Home/Hero'
-import React from 'react'
+import BestSellingProducts from "@/components/Home/BestSelling";
+import CategoriesSection from "@/components/Home/Categories";
+import Hero from "@/components/Home/Hero";
+import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <Hero/>
-      <CategoriesSection/>
-      <BestSellingProducts/>
+      <Hero />
+      <CategoriesSection />
+      <BestSellingProducts />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
