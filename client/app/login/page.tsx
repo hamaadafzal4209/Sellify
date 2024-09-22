18c237fde8a21c1edcc13feeda23f9f50fe0f9c9
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-21 22:30:30 -0700

    commit

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -103,7 +103,7 @@ const SignInPage = () => {
 
         <div className="relative flex items-center justify-center my-8">
           <div className="w-full h-[1px] bg-gray-300"></div>
-          <span className="px-4 text-sm font-medium text-gray-500 bg-white mx-4">
+          <span className="px-4 text-sm font-medium text-gray-500 mx-2">
             or
           </span>
           <div className="w-full h-[1px] bg-gray-300"></div>
