471246a691b436c9c4e87dc41861e28d826822d6
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-22 17:22:02 -0700

    create user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,32 +2,66 @@ import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
   {
-    username: {
+    name: {
       type: String,
-      required: true,
-      unique: true,
+      required: [true, "Please enter your name"],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "Please enter your email"],
+      validate: {
+        validator: function (value) {
+          return emailRegexPattern.test(value);
+        },
+        message: "Please enter a valid email",
+      },
       unique: true,
     },
     password: {
       type: String,
-      required: true,
+      minlength: [6, "Password must be at least 6 characters"],
+      select: false,
     },
-    profilePicture: {
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
       type: String,
-      default:
-        "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png",
+      default: "user",
     },
     isAdmin: {
       type: Boolean,
       default: false,
     },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
   },
   { timestamps: true }
 );
+
+//  Hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// jwt token
+userSchema.methods.getJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRES,
+  });
+};
+
+// compare password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const userModel = mongoose.model("User", userSchema);
 export default userModel;
