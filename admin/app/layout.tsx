332906eea5bc805bd5e7595e9fb839bc694de533
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-27 20:51:10 -0700

    admin panel initialize

---
+++ admin/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Inter, Poppins } from "next/font/google";
+import Header from "@/components/Layout/Header";
+import Footer from "@/components/Layout/Footer";
+import { cn } from "@/lib/utils";
+import { Toaster } from "react-hot-toast";
+
+// Load the Inter font for body text
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+// Load the Poppins font for headings
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800"],
+  variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+  title: "Sellify - Admin",
+  description:
+    "Discover the best products at unbeatable prices with Sellify, your go-to eCommerce platform.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cn(
+        "min-h-screen bg-dark-300 font-sans antialiased",
+        inter.variable,
+        poppins.variable
+      )}
+    >
+      <body className="font-poppins">
+          <Toaster />
+          <Header />
+          {children}
+          <Footer />
+      </body>
+    </html>
+  );
+}
