04b852ae312a73fa6b6b59082e6679068d259416
FrontendFlair <hamaadafzal404@gmail.com>
2024-09-30 17:56:18 -0700

    complete category api route

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -1,33 +1,39 @@
 import cloudinary from "cloudinary";
+import Category from "../models/categoryModel.js";
 import catchAsyncErrors from "../middleware/catchAsyncErrors.js";
 import ErrorHandler from "../utils/ErrorHandler.js";
-import Category from "../models/categoryModel.js";
 
-// Create Category 
+// Create Category with Cloudinary Image Upload
 export const createCategory = catchAsyncErrors(async (req, res, next) => {
-  const { name } = req.body;
+  const { name, imageBase64 } = req.body;
 
+  // Check if the name is provided
   if (!name) {
     return next(new ErrorHandler("Category name is required", 400));
   }
 
-  if (!req.file) {
-    return next(new ErrorHandler("Please upload an image", 400));
+  // Check if the image is provided
+  if (!imageBase64) {
+    return next(new ErrorHandler("Please provide an image in Base64 format", 400));
   }
 
   try {
     // Upload image to Cloudinary
-    const result = await cloudinary.v2.uploader.upload(req.file.path, {
+    const result = await cloudinary.v2.uploader.upload(imageBase64, {
       folder: "categories",
       width: 500,
       crop: "scale",
     });
 
-    // Create new category with image details
+    // Create new category with the Cloudinary image URLs
     const newCategory = await Category.create({
       name,
-      imageUrl: result.secure_url, // Cloudinary secure URL
-      public_id: result.public_id,  // Cloudinary public_id
+      image: [
+        {
+          public_url: result.secure_url, // Cloudinary secure URL
+          url: result.url, // Cloudinary URL
+        },
+      ],
     });
 
     // Respond with success
