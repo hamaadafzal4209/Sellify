ce9d6f9220e0f6ecbb096d7a550d22516e961d21
FrontendFlair <hamaadafzal404@gmail.com>
2024-10-01 12:46:37 -0700

    commit

--- admin/app/categories/page.tsx
+++ admin/app/categories/page.tsx
@@ -5,15 +5,35 @@ import React, { useEffect, useState } from "react";
 import { Plus, Pencil, Trash2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
-import { Dialog, DialogContent, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 import { Label } from "@/components/ui/label";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { useDispatch, useSelector } from "react-redux";
 import Image from "next/image";
 import { toast } from "react-hot-toast";
-import { createCategory, deleteCategory, fetchAllCategories, updateCategory } from "../redux/Features/category/categoryAction";
+import {
+  createCategory,
+  deleteCategory,
+  fetchAllCategories,
+  updateCategory,
+} from "../redux/Features/category/categoryAction";
 
 export default function CategoriesPage() {
   const dispatch = useDispatch();
@@ -53,7 +73,9 @@ export default function CategoriesPage() {
     setLoading(true);
     const reader = new FileReader();
     reader.onloadend = () => {
-      dispatch(updateCategory(currentCategory._id, newCategoryName, reader.result));
+      dispatch(
+        updateCategory(currentCategory._id, newCategoryName, reader.result)
+      );
       toast.success("Category updated successfully!");
       setLoading(false);
       setCurrentCategory(null);
@@ -81,13 +103,15 @@ export default function CategoriesPage() {
   };
 
   return (
-    <div className="max-w-3xl w-full mx-auto p-4">
+    <div className="max-w-3xl w-full mx-auto p-4 pb-12">
       <div className="flex justify-between items-center mb-4">
-        <h1 className="text-3xl font-semibold text-gray-800">Categories</h1>
+        <h1 className="text-xl md:text-3xl font-semibold text-gray-800">
+          Categories
+        </h1>
         <Dialog>
           <DialogTrigger asChild>
             <Button className="bg-main-500 text-white hover:bg-main-600">
-              <Plus className="mr-2" />
+              <Plus className="mr-2  hidden sm:block" />
               Add Category
             </Button>
           </DialogTrigger>
@@ -112,7 +136,11 @@ export default function CategoriesPage() {
               className="mb-4"
             />
             <DialogFooter>
-              <Button onClick={handleCreateCategory} disabled={loading} className="bg-blue-600 text-white hover:bg-blue-700">
+              <Button
+                onClick={handleCreateCategory}
+                disabled={loading}
+                className="bg-blue-600 text-white hover:bg-blue-700"
+              >
                 {loading ? "Creating..." : "Create"}
               </Button>
             </DialogFooter>
@@ -129,66 +157,68 @@ export default function CategoriesPage() {
       />
 
       <ScrollArea>
-        <Table className="border">
-          <TableHeader>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead>Image</TableHead>
-              <TableHead>Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {allCategories
-              .filter((category) =>
-                category.name.toLowerCase().includes(searchTerm.toLowerCase())
-              )
-              .map((category) => (
-                <TableRow key={category._id} className="hover:bg-gray-100">
-                  <TableCell className="py-2">{category.name}</TableCell>
-                  <TableCell className="py-2">
-                    <Image src={category.image[0].public_url} alt={category.name} width={50} height={50} className="rounded" />
-                  </TableCell>
-                  <TableCell className="py-2">
-                    <Button
-                      variant="outline"
-                      onClick={() => {
-                        setCurrentCategory(category);
-                        setNewCategoryName(category.name);
-                        setIsEditDialogOpen(true);
-                      }}
-                      className="mr-2"
-                    >
-                      <Pencil className="mr-1" /> Edit
-                    </Button>
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <Button variant="outline" color="red">
-                          <Trash2 className="mr-1" /> Delete
-                        </Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent>
-                        <AlertDialogHeader>
-                          <AlertDialogTitle>
-                            Are you sure you want to delete this category?
-                          </AlertDialogTitle>
-                        </AlertDialogHeader>
-                        <AlertDialogFooter>
-                          <AlertDialogCancel onClick={() => setIsDeleteDialogOpen(false)}>
-                            Cancel
-                          </AlertDialogCancel>
-                          <AlertDialogAction
-                            onClick={() => handleDeleteCategory(category._id)}
-                          >
-                            Confirm
-                          </AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
+        <div className="flex flex-wrap">
+          {allCategories
+            .filter((category) =>
+              category.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            .map((category) => (
+              <div
+                key={category._id}
+                className="flex items-center border-b py-2 w-full justify-between"
+              >
+                <div className="flex items-center gap-2">
+                  <Image
+                    src={category.image[0].public_url}
+                    alt={category.name}
+                    width={50}
+                    height={50}
+                    className="rounded w-12 h-12"
+                  />
+                  <span className="text-base md:text-lg">{category.name}</span>
+                </div>
+                <div className="flex items-center">
+                  <Button
+                    variant="outline"
+                    onClick={() => {
+                      setCurrentCategory(category);
+                      setNewCategoryName(category.name);
+                      setIsEditDialogOpen(true);
+                    }}
+                    className="mr-2"
+                  >
+                    <Pencil className="mr-1" />
+                  </Button>
+                  <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                      <Button variant="outline" color="red">
+                        <Trash2 className="mr-1" />
+                      </Button>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <AlertDialogTitle>
+                          Are you sure you want to delete this category?
+                        </AlertDialogTitle>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogCancel
+                          onClick={() => setIsDeleteDialogOpen(false)}
+                        >
+                          Cancel
+                        </AlertDialogCancel>
+                        <AlertDialogAction
+                          onClick={() => handleDeleteCategory(category._id)}
+                        >
+                          Confirm
+                        </AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                </div>
+              </div>
+            ))}
+        </div>
       </ScrollArea>
 
       {/* Edit Category Dialog */}
@@ -214,7 +244,11 @@ export default function CategoriesPage() {
             className="mb-4"
           />
           <DialogFooter>
-            <Button onClick={handleUpdateCategory} disabled={loading} className="bg-blue-600 text-white hover:bg-blue-700">
+            <Button
+              onClick={handleUpdateCategory}
+              disabled={loading}
+              className="bg-blue-600 text-white hover:bg-blue-700"
+            >
               {loading ? "Updating..." : "Update"}
             </Button>
           </DialogFooter>
