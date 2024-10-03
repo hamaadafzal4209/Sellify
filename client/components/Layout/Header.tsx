bc766feaa7671f1712b1995530c4d63f1927d652
FrontendFlair <hamaadafzal404@gmail.com>
2024-10-03 08:03:22 -0700

    commit

--- client/components/Layout/Header.tsx
+++ client/components/Layout/Header.tsx
@@ -181,7 +181,7 @@ export default function Header() {
                         <Link href="/login" passHref>
                           <Button
                             variant="ghost"
-                            className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-main-500 hover:bg-main-600"
+                            className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-main-500 hover:bg-main-600 hover:text-white"
                           >
                             Login
                           </Button>
@@ -250,7 +250,7 @@ export default function Header() {
               <Link href="/login" passHref>
                 <Button
                   variant="ghost"
-                  className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-main-500 hover:bg-main-600"
+                  className="whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-main-500 hover:bg-main-600 hover:text-white"
                 >
                   Login
                 </Button>
