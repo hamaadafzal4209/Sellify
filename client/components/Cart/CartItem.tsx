f33dab861e3810263e4b9bb3442e4b6c5fc9321f
FrontendFlair <hamaadafzal404@gmail.com>
2024-10-04 03:40:29 -0700

    create cart page

--- client/components/Cart/CartItem.tsx
+++ client/components/Cart/CartItem.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
-const CartItem: React.FC<{ item: any }> = ({ item }) => {
+const CartItem: React.FC<{
+  item: any;
+  onQuantityChange: (id: string, increment: boolean) => void;
+}> = ({ item, onQuantityChange }) => {
   return (
     <div>
       <div className="rounded-lg border border-gray-200 bg-white p-4 shadow-sm dark:border-gray-700 dark:bg-gray-800 md:p-6">
         <div className="space-y-4 md:flex md:items-center md:justify-between md:gap-6 md:space-y-0">
-          <a href="#" className="shrink-0 md:order-1">
-            <img
-              className="h-20 w-20"
-              src={item.image} // assuming 'image' is a key in the item object
-              alt={item.name} // assuming 'name' is a key in the item object
-            />
-          </a>
+          <Image
+            width={20}
+            height={20}
+            className="h-20 w-20"
+            src={item.images[0]?.url}
+            alt={item.name}
+          />
 
           <label htmlFor="counter-input" className="sr-only">
             Choose quantity:
@@ -21,21 +25,23 @@ const CartItem: React.FC<{ item: any }> = ({ item }) => {
               <button
                 type="button"
                 id="decrement-button"
-                className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                className="inline-flex h-5 w-5 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none dark:border-gray-600 dark:bg-gray-700"
+                onClick={() => onQuantityChange(item.id, false)}
               >
                 -
               </button>
               <input
                 type="text"
                 id="counter-input"
-                className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none focus:ring-0 dark:text-white"
+                className="w-10 shrink-0 border-0 bg-transparent text-center text-sm font-medium text-gray-900 focus:outline-none dark:text-white"
                 value={item.quantity}
                 readOnly
               />
               <button
                 type="button"
                 id="increment-button"
-                className="inline-flex h-5 w-5 shrink-0 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+                className="inline-flex h-5 w-5 items-center justify-center rounded-md border border-gray-300 bg-gray-100 hover:bg-gray-200 focus:outline-none dark:border-gray-600 dark:bg-gray-700"
+                onClick={() => onQuantityChange(item.id, true)}
               >
                 +
               </button>
@@ -47,7 +53,7 @@ const CartItem: React.FC<{ item: any }> = ({ item }) => {
               {item.name}
             </p>
             <p className="text-sm text-gray-500 dark:text-gray-400">
-              ${item.price.toFixed(2)}
+              ${item.discountPrice.toFixed(2)}
             </p>
           </div>
         </div>
